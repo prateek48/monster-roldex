066e37bdcdbe74e033a3861291b27f2cc15876d8
prateek48 <60612561+prateek48@users.noreply.github.com>
2021-05-01 13:43:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{ Component } from 'react';
+import ReactDom from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  var mosters=fetch('https://jsonplaceholder.typicode.com/users').then(response=>response.json());
+  render(){
+    return (<h1>mosters.user</h1>);
+  }
 }
 
 export default App;
